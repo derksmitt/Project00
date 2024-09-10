2e8747823543c07953d586236646dc2b874528c2
derksmitt <derek.school.acc@gmail.com>
2024-09-09 22:51:46 -0700

    changed age

--- SimpleObject.java
+++ SimpleObject.java
@@ -1,7 +1,7 @@
 public class SimpleObject {
     public static void main(String[] args) {
         // Create an instance of the Person class
-        Person person1 = new Person("Joe", 30);
+        Person person1 = new Person("Joe", 50);
 
         // Display the person's information
         System.out.println(person1.toString());
