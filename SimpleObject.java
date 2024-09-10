d2886874ededd4ee047776049a17cf2398d7a8d5
derksmitt <derek.school.acc@gmail.com>
2024-09-09 22:39:57 -0700

    initial commit

---
+++ SimpleObject.java
@@ -0,0 +1,9 @@
+public class SimpleObject {
+    public static void main(String[] args) {
+        // Create an instance of the Person class
+        Person person1 = new Person("Alice", 30);
+
+        // Display the person's information
+        System.out.println(person1.toString());
+    }
+}
